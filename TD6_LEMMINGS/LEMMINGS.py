629b8866f23d719f2d4fbf10e6f003a4ce0ef564
Berachem Markria <berachem.markria@gmail.com>
2024-03-26 09:35:59 +0100

    clean up comments & features list

--- TD6_LEMMINGS/LEMMINGS.py
+++ TD6_LEMMINGS/LEMMINGS.py
@@ -1,3 +1,20 @@
+"""
+By Berachem MARKRIA & Joshua LEMOINE
+
+TD6 - Lemmings
+Ce programme est un exemple simplifié du jeu Lemmings en utilisant Pygame. 
+
+Features BONUS à LEMMINGS :
+- Ajout du sort : Parachutiste
+- Ajout du sort : explosion
+- Ajout de la fonctionnalité : pause, exit
+"""
+
+import pygame
+import os, inspect
+import random
+
+# Rest of the code...
 import pygame
 import os, inspect
 import random
@@ -76,6 +93,9 @@ actions = [
 ]
 
 def ChargeSerieSprites(id):
+    """
+    Renvoie une liste de sprites à partir d'une planche de sprites.
+    """
     sprite = []
     for i in range(18):
         spr = planche_sprites.subsurface((LARG * i, LARG * id, LARG, LARG))
@@ -95,45 +115,67 @@ explosion = ChargeSerieSprites(5)
 # ==============================================================================
 # Fonctions de gestion des actions des Lemmings
 # ==============================================================================
-def actionMarche(lemming) :
-   lemming["x"]+=lemming["vx"]
+def actionMarche(lemming):
+    """
+    Action de déplacement du Lemming.
+    """
+    lemming["x"] += lemming["vx"]
+    
 
 def actionChute(lemming) :
-   lemming['y'] += 3
-   lemming['fallcount'] += 3
+    """ 
+    Action de chute du Lemming.
+    """
+    lemming['y'] += 3
+    lemming['fallcount'] += 3
  
 def actionStop(lemming) :
    return 
 
 def actionMort(lemming) :
-   if(lemming['deathCounter'] != 0):
-      lemming['deathCounter']-=1
-   else:
-      lemmingsLIST.remove(lemming)
+    """ 
+    Action de mort du Lemming.
+    """
+    if(lemming['deathCounter'] != 0):
+        lemming['deathCounter']-=1
+    else:
+        lemmingsLIST.remove(lemming)
 
 def actionExplosion(lemming) :
-   if(lemming['explosionCounter'] != 0):
-      lemming['explosionCounter']-=1
-   else:
-      x = lemming["x"] + int(lemmingWidth/2)
-      y = lemming["y"] + int(lemmingHeight/2)
-      for i in range(60):
-         for j in range(60):
-            fond.set_at(( x - int(60/2) + i, y - int(60/2) + j),BLACK)
-      lemmingsLIST.remove(lemming)
+    """ 
+    Action d'explosion du Lemming.
+    """
+    if(lemming['explosionCounter'] != 0):
+        lemming['explosionCounter']-=1
+    else:
+        x = lemming["x"] + int(lemmingWidth/2)
+        y = lemming["y"] + int(lemmingHeight/2)
+        for i in range(60):
+            for j in range(60):
+                fond.set_at(( x - int(60/2) + i, y - int(60/2) + j),BLACK)
+        lemmingsLIST.remove(lemming)
 
 def actionCreuse(lemming) :
-   for i in range(20) :
-      fond.set_at((lemming["x"] + i,lemming["y"] + lemmingHeight),BLACK)
-   lemming["y"]+=1
+    """ 
+    Action de creusement du Lemming.
+    """
+    for i in range(20) :
+        fond.set_at((lemming["x"] + i,lemming["y"] + lemmingHeight),BLACK)
+    lemming["y"]+=1
 
 def actionParachute(lemming) :
-   lemming['y'] += 1
+    """ 
+    Action de chute du Lemming.
+    """
+    lemming['y'] += 1
    
 # ==============================================================================
 # Fonctions de gestion des transitions d'états des Lemmings
 # ==============================================================================
-def transitionChute(lemming):
+def transitionChute(lemming): 
+    """
+    Transition de l'état Chute.
+    """
     if lemming["y"] + lemmingHeight >= WINDOW_SIZE[1]:  # Si le lemming dépasse le bas de l'écran
         lemming["etat"] = EtatDead
         return
@@ -146,69 +188,84 @@ def transitionChute(lemming):
           lemming["etat"] = EtatMarche
 
 def transitionMarche(lemming):
+    """
+    Transition de l'état Marche.
+    """
 
-   #colision sol
+    #colision sol
 
-   #on  recentre le point au millieu des pied
-   if(fond.get_at((lemming["x"] + int(lemmingWidth/2),lemming["y"] + lemmingHeight)) == BLACK):
-      lemming["etat"]=EtatChute
-      return
-   
-   #colision lemming
+    #on  recentre le point au millieu des pied
+    if(fond.get_at((lemming["x"] + int(lemmingWidth/2),lemming["y"] + lemmingHeight)) == BLACK):
+        lemming["etat"]=EtatChute
+        return
+    
+    #colision lemming
 
-   #on  check tout les lemmings pour voir si il y en à un en arret
-   for onelemming in lemmingsLIST :
-      if(onelemming["etat"] == EtatStop):
-         #on recentre les points pour plus de facilité
-         x1 = onelemming["x"] 
-         y1 = onelemming["y"] 
-         x2 = lemming["x"]
-         y2 = lemming["y"]
+    #on  check tout les lemmings pour voir si il y en à un en arret
+    for onelemming in lemmingsLIST :
+        if(onelemming["etat"] == EtatStop):
+            #on recentre les points pour plus de facilité
+            x1 = onelemming["x"] 
+            y1 = onelemming["y"] 
+            x2 = lemming["x"]
+            y2 = lemming["y"]
 
-         if(((x1-x2)**2 + (y1-y2)**2) < lemmingWidth*8) : #largeur d'un lemming en position stop
-            lemming["vx"]*=-1
+            if(((x1-x2)**2 + (y1-y2)**2) < lemmingWidth*8) : #largeur d'un lemming en position stop
+                lemming["vx"]*=-1
 
-            return
+                return
 
-   #collision murale :
+    #collision murale :
 
-   #on  recentre le point en bas à gauche, 5 pixels devant
-   #-5 en y pour que l'action de tomber dans un trou d'un autre lemming soit logique
-   if(fond.get_at((lemming["x"] + 5,lemming["y"] + lemmingHeight - 5)) != BLACK): 
-      lemming["vx"]*=-1
-      return
+    #on  recentre le point en bas à gauche, 5 pixels devant
+    #-5 en y pour que l'action de tomber dans un trou d'un autre lemming soit logique
+    if(fond.get_at((lemming["x"] + 5,lemming["y"] + lemmingHeight - 5)) != BLACK): 
+        lemming["vx"]*=-1
+        return
 
-   #on  recentre le point en bas à droite, 5 pixels devant
-   if(fond.get_at((lemming["x"] + lemmingWidth + 5,lemming["y"] + lemmingHeight - 5)) != BLACK):
-      lemming["vx"]*=-1
-      return
+    #on  recentre le point en bas à droite, 5 pixels devant
+    if(fond.get_at((lemming["x"] + lemmingWidth + 5,lemming["y"] + lemmingHeight - 5)) != BLACK):
+        lemming["vx"]*=-1
+        return
 
-   #sortie 
-   if((lemming["x"] + lemmingWidth) >= (sortieX+ int(sortieWidth/2)) and (lemming["y"] + int(lemmingHeight/2)) >= (sortieY + int(sortieHeight/2))):
-      lemmingsAlive.append(lemming)
-      lemmingsLIST.remove(lemming)
+    #sortie 
+    if((lemming["x"] + lemmingWidth) >= (sortieX+ int(sortieWidth/2)) and (lemming["y"] + int(lemmingHeight/2)) >= (sortieY + int(sortieHeight/2))):
+        lemmingsAlive.append(lemming)
+        lemmingsLIST.remove(lemming)
 
 def transitionCreuse(lemming) :
-   compteur = 0
-   for i in range(20) :
-      if lemming["y"] + lemmingHeight >= WINDOW_SIZE[1]:  # Si le lemming dépasse le bas de l'écran
-         lemming["etat"] = EtatDead
-         break
-      if(fond.get_at((lemming["x"] + i,lemming["y"] + lemmingHeight)) == BLACK) :
-         compteur += 1
-   if(compteur == 20 ):
-      lemming["etat"] = EtatMarche
+    """
+    Transition de l'état Creuse.
+    """
+    compteur = 0
+    for i in range(20) :
+        if lemming["y"] + lemmingHeight >= WINDOW_SIZE[1]:  # Si le lemming dépasse le bas de l'écran
+            lemming["etat"] = EtatDead
+            break
+        if(fond.get_at((lemming["x"] + i,lemming["y"] + lemmingHeight)) == BLACK) :
+            compteur += 1
+    if(compteur == 20 ):
+        lemming["etat"] = EtatMarche
 
    
 
 
 def transitionParachute(lemming) :
-   if(fond.get_at((lemming["x"] + int(lemmingWidth/2),lemming["y"] + lemmingHeight)) != BLACK):
-      lemming["fallcount"] = 0 #pour être sur qu'il n'y a paas d'accumulation de fall damages
-      lemming["etat"] = EtatMarche
+    """
+    Transition de l'état Parachute.
+    """
+    if(fond.get_at((lemming["x"] + int(lemmingWidth/2),lemming["y"] + lemmingHeight)) != BLACK):
+        lemming["fallcount"] = 0 #pour être sur qu'il n'y a paas d'accumulation de fall damages
+        lemming["etat"] = EtatMarche
 
 
 def handle_events():
+    """
+    Fonction de gestion des événements, clavier et souris.
+     - Espace ou P pour mettre en pause
+     - Clic gauche pour sélectionner une action
+     - Echap pour quitter
+    """
     global done, is_paused
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -217,11 +274,16 @@ def handle_events():
             if event.key == pygame.K_SPACE or event.key == pygame.K_p:
                 print(is_paused and "Unpaused" or "Paused")
                 is_paused = not is_paused
+            elif event.key == pygame.K_ESCAPE:
+                done = True
         elif event.type == pygame.MOUSEBUTTONDOWN and not is_paused:
             handle_mouse_event(pygame.mouse.get_pos())
 
 
 def handle_mouse_event(pos):
+    """
+    Fonction de gestion des événements de la souris.
+    """
     global selected_rectangle, selected_action, compteur_waiting_lemming
     x, y = pos
     x = pos[0]
@@ -255,6 +317,10 @@ def handle_mouse_event(pos):
                             compteur_waiting_lemming+=1 
 
 def create_lemmings():
+    """ 
+    Création des Lemmings.
+    Chaque 15 ticks, un nouveau Lemming est créé et ajouté à la liste des Lemmings.
+    """
     global compteur_creation
     time = int(pygame.time.get_ticks() / 100)
     if compteur_creation < 15 and (time + compteur_creation) % 15 == 0:
@@ -264,6 +330,9 @@ def create_lemmings():
         lemmingsLIST.append(new_lemming)
         
 def update_and_draw_lemmings():
+    """
+    Mise à jour et affichage des Lemmings.
+    """
     for lemming in lemmingsLIST[:]:
         update_lemming_state(lemming)
         perform_lemming_action(lemming)
@@ -271,6 +340,10 @@ def update_and_draw_lemmings():
     
 
 def update_lemming_state(lemming):
+    """
+    Fonction de mise à jour de l'état du Lemming.
+    Fonctionne en fonction de l'état du Lemming.
+    """
     if(lemming['etat'] == EtatChute):
          transitionChute(lemming)
     elif(lemming['etat'] == EtatMarche):
@@ -281,6 +354,11 @@ def update_lemming_state(lemming):
          transitionParachute(lemming)
 
 def perform_lemming_action(lemming):
+    """
+    Fonction de gestion des actions du Lemming. 
+    Fonctionne en fonction de l'état du Lemming.
+    Cette fonction est appelée à chaque tick.
+    """
     global time
     if(lemming['etat'] == EtatMarche):
         actionMarche(lemming)
@@ -299,6 +377,10 @@ def perform_lemming_action(lemming):
         actionExplosion(lemming)
 
 def draw_lemming(lemming):
+    """
+    Fonction d'affichage du Lemming.
+    Fonctionne en fonction de l'état du Lemming.
+    """
     time = pygame.time.get_ticks() // 100
     decal = lemming['decal']
     spr = None
@@ -334,6 +416,10 @@ def draw_lemming(lemming):
 
 
 def check_game_over():
+    """
+    Fonction de vérification de la fin de partie.
+    Affiche un message de victoire ou de défaite.
+    """
     if compteur_creation == 15 and (len(lemmingsLIST) == 0 or len(lemmingsLIST) == compteur_waiting_lemming):
         if len(lemmingsAlive) >= 10:
             screen.blit(victoryText, victoryText.get_rect(center=(WINDOW_SIZE[0] / 2, WINDOW_SIZE[1] / 2)))
@@ -351,19 +437,20 @@ while not done:
     time = int(pygame.time.get_ticks() / 100)
     handle_events()
     
-    if not is_paused:
+    if not is_paused: # Jeu en cours
         create_lemmings()
-        screen.blit(fond, (0, 0))  
-        screen.blit(sortie, (sortieX, sortieY))
-        update_and_draw_lemmings()
+        screen.blit(fond, (0, 0))  # Affichage du fond
+        screen.blit(sortie, (sortieX, sortieY)) # Affichage de la sortie
+        update_and_draw_lemmings() # Mise à jour et affichage des Lemmings
         if selected_rectangle is not None:
-            pygame.draw.rect(screen, YELLOW, selected_rectangle)
+            pygame.draw.rect(screen, YELLOW, selected_rectangle) # Affichage de la sélection d'action
         check_game_over()
-    else:
+    else: # Pause
         screen.blit(pauseText, pauseText.get_rect(center=(WINDOW_SIZE[0] / 2, WINDOW_SIZE[1] / 2)))
 
     pygame.display.flip()
     clock.tick(20)
 
+print("Exiting...")
 pygame.quit()
 
